96c74a3cb01a19d5e3dd08756e678953585bf9af
FernandoJVideira <03.pleaser-minster@icloud.com>
2026-09-03 10:52:03 +0100

    fix(macos): restore charging icon in sketchybar battery plugin
    A prior fix for the BSD-grep percentage-parsing bug dropped AC-power
    detection along with it. Reintroduce the charging check and swap in a
    level-matched charging icon when plugged in.

--- macos/.config/sketchybar/plugins/battery.sh
+++ macos/.config/sketchybar/plugins/battery.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-PERCENTAGE=$(pmset -g batt | grep -o '[0-9]\+%' | cut -d% -f1)
+BATT_INFO=$(pmset -g batt)
+PERCENTAGE=$(echo "$BATT_INFO" | grep -o '[0-9]\+%' | cut -d% -f1)
+CHARGING=$(echo "$BATT_INFO" | grep 'AC Power')
 
 if [[ -z "$PERCENTAGE" ]]; then
     exit 0
@@ -8,26 +10,36 @@ fi
 
 case ${PERCENTAGE} in
 9[0-9] | 100)
-    ICON=""
+    ICON=""
     ICON_COLOR="0xff40a02b"
     ;;
 [6-8][0-9])
-    ICON=""
+    ICON=""
     ICON_COLOR="0xff179299"
     ;;
 [3-5][0-9])
-    ICON=""
+    ICON=""
     ICON_COLOR="0xfffe640b"
     ;;
 [1-2][0-9])
-    ICON=""
+    ICON=""
     ICON_COLOR="0xffdf8e1d"
     ;;
 *)
-    ICON=""
+    ICON=""
     ICON_COLOR="0xffd20f39"
     ;;
 esac
+
+if [[ -n "$CHARGING" ]]; then
+  case ${PERCENTAGE} in
+  9[0-9] | 100) ICON="󰂅" ;;
+  [6-8][0-9])   ICON="󰂊" ;;
+  [3-5][0-9])   ICON="󰢝" ;;
+  [1-2][0-9])   ICON="󰂆" ;;
+  *)            ICON="󰢟" ;;
+  esac
+fi
 
 sketchybar --set "$NAME" \
     icon="$ICON" \
